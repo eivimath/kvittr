e26c8f5999893426a971952e04df937541164e79
Eivind Mathisen <eivimath@gmail.com>
2015-04-27 12:42:00 +0200

    Modifyed views.py in useraccounts to contain login, logout and support for existing username and email errors.

--- kvittr/useraccounts/views.py
+++ kvittr/useraccounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-
-# Create your views here.
 
 def user_register(request):
 	context = {}
@@ -9,9 +9,34 @@ def user_register(request):
 		user = User()
 		user.first_name = request.POST.get('firstname')
 		user.last_name = request.POST.get('lastname')
-		user.username = request.POST.get('username')
-		user.email = request.POST.get('email')
 		user.set_password(request.POST.get('password'))
-		user.save()
-		context['user_saved_successfully'] = True
+		if User.objects.filter(username=request.POST['username']).exists():
+			context['info_exist'] = True
+		elif User.objects.filter(email=request.POST['email']).exists():
+			context['email_exist'] = True
+		else:
+			user.username = request.POST.get('username')
+			user.email = request.POST.get('email')
+			user.save()
+			context['user_saved_successfully'] = True
 	return render(request, 'useraccounts/register.html', context)
+
+def user_login (request):
+	context = {}
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('frontpage')
+		else:
+			context['login_failed'] = True
+	return render(request, 'useraccounts/login.html', context)
+
+def user_logout(request):
+	logout(request)
+	return redirect('frontpage')
+
+def user_profile(request):
+	return render(request, 'useraccounts/user_profile.html')
