9851af56db4e0e24b25ea179867c4a32bb991b79
Eivind Mathisen <eivimath@gmail.com>
2015-04-27 13:24:42 +0200

    Corrected a misspelling in urls.py in messages

--- kvittr/messages/urls.py
+++ kvittr/messages/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from kvittr_messages import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.message_listing, name='message_listing'),
+	url(r'^$', views.message_listing, name='messages'),
 )
