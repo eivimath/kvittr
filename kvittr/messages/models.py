c566ba16e0928b66a5eda6d2c6743ee063b4f7c0
Eivind Mathisen <eivimath@gmail.com>
2015-04-27 13:18:08 +0200

    Added functions for date and time, and created by in models.py in messages.

--- kvittr/messages/models.py
+++ kvittr/messages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Message(models.Model):
+	message = models.TextField()
+	created_by = models.ForeignKey(User, related_name="created_message")
+	created_datetime = models.DateTimeField()
+
+	def __unicode__(self):
+		return u'%s' % self.message
+
+	class Meta:
+		ordering = ['-id']
