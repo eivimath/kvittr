29b6024777dd1a7907e1e2610038c90fb2585124
Eivind Mathisen <eivimath@gmail.com>
2015-05-04 13:29:56 +0200

    Added render for messages_display in views.py in theme

--- kvittr/theme/views.py
+++ kvittr/theme/views.py
@@ -3,3 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def frontpage(request):
     return render(request, 'theme/frontpage.html')
+    return render(request, 'messages_display/templates/messages_display/messages.html')
