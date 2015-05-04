1e6157fab09ec5448fc3c74aa5b07ac2cc0ff6ec
Eivind Mathisen <eivimath@gmail.com>
2015-05-04 13:38:39 +0200

    Added pagination to viwes in messages_display

--- kvittr/messages_display/views.py
+++ kvittr/messages_display/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.utils import timezone
 from messages_display.models import Message
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -16,6 +17,15 @@ def messages(request):
 		new_message.save()
 
 	messages = Message.objects.all()
+	page_number = request.GET.get('page')
+	paginator = Paginator(messages, 5)
+	try:
+		messages = paginator.page(page_number)
+	except PageNotAnInteger:
+		messages = paginator.page(1)
+	except EmptyPage:
+		messages = paginator.page(paginator.num_pages)
+
 	context = {'messages': messages}
 	return render(request, 'messages_display/messages.html', context)
 
