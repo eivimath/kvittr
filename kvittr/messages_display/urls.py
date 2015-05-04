437f1329da69aacc4ce550abaf894d954ee3a522
Eivind Mathisen <eivimath@gmail.com>
2015-05-04 13:26:42 +0200

    No changes made to urls.py in messages_display

--- kvittr/messages_display/urls.py
+++ kvittr/messages_display/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-
 from messages_display import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.messages, name='messages'),
+	url(r'^(\d+)/increase_likes$', views.like_button, 
+		name='like_button'),
 )
