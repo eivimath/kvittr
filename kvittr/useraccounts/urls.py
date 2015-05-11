f92ffe0dd30b56c28e63d50c1b1391478f02c0a3
Eivind Mathisen <eivimath@gmail.com>
2015-05-11 12:31:23 +0200

    Added url pattern for user edit prfile

--- kvittr/useraccounts/urls.py
+++ kvittr/useraccounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
 	url(r'^login$', views.user_login, name='user_login'),
 	url(r'^logout$', views.user_logout, name='user_logout'),
 	url(r'^user_profile$', views.user_profile, name='user_profile'),
+	url(r'^edit_profile$', views.user_edit_profile, name='user_edit_profile'),
 	)
