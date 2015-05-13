7f8cdeca93397b904ad5ef81d9c99c6256ee254d
Eivind Mathisen <eivimath@gmail.com>
2015-05-13 14:22:30 +0200

    Added defenition users and messages to views.py

--- kvittr/theme/views.py
+++ kvittr/theme/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def frontpage(request):
     return render(request, 'theme/frontpage.html')
-    return render(request, 'messages_display/templates/messages_display/messages.html')
+
+def messages(request):
+	return render(request, 'messages_display/templates/messages_display/messages.html')
+
+def users(request):
+    return render(request, 'theme/users.html')
