12aeaaafeb9ff68cc1a6dad83a5b8a62f16bc61e
Eivind Mathisen <eivimath@gmail.com>
2015-05-13 11:57:16 +0200

    Added a url pattern to urls.py to contain detailed message.

--- kvittr/messages_display/urls.py
+++ kvittr/messages_display/urls.py
@@ -4,4 +4,5 @@ from messages_display import views
 urlpatterns = patterns('',
 	url(r'^$', views.messages, name='messages'),
 	url(r'^(\d+)/add_likes$', views.add_likes, name='add_likes'),
+	url(r'^(\d+)/$', views.detailed_message, name='detailed_message'),
 )
